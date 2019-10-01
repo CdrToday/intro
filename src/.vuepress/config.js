d5d5168bab9613e86dfc2212b356e562e95f5c8f
clearloop <udtrokia@gmail.com>
2019-10-01 10:23:36 +0800

    add privacy

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -5,25 +5,27 @@ module.exports = {
   themeConfig: {
     sidebar: [
       '/', 
-      // {
-      // 	// title: '设计',
-      // 	// path: '/arch/',
-      // 	// children: [
-      // 	  // '/arch/funcs',
-      // 	  // '/arch/status'
-      // 	],
-      // },
+      '/download',
+      {
+	title: '隐私',
+	// path: '/privacy/',
+	collapsable: false,
+        sidebarDepth: 0,
+	children: [
+	  '/privacy/zh',
+	  '/privacy/en'
+	]
+      },
       {
 	title: '版本',
 	// path: '/versions/',
+	collapsable: false,
 	children: [
 	  '/versions/v0.1.x',
 	  '/versions/v0.2.x',
 	]
       },
-      '/download'
     ],
-    
-    displayAllHeaders: true
+    displayAllHeaders: false
   }
 }
